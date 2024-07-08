acf0821a87ded426afe59647e578464f840eff43
Anka <runner@Mac-1720443230555.local>
2024-07-08 13:06:38 +0000

    Bump to version 2.37.5 (matrix-rich-text-editor/HEAD 56686017e65c62b9dc5d2b89ec71d158efdb2967)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "d5665109cf433ca1746d301711eae77bbc7c3f1698a86a17ddd4a54892b75b1e"
-let version = "2.37.4"
+let checksum = "ba792c8ebd0e37176daff20555a8a4878ab556aa37b052da3eacd39a95ece673"
+let version = "2.37.5"
 let url = "https://github.com/matrix-org/matrix-rich-text-editor-swift/releases/download/\(version)/WysiwygComposerFFI.xcframework.zip"
 let package = Package(
     name: "WysiwygComposer",
